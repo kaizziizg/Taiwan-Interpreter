82ce829d29f616e9659d0c827e5d6e5596f68592
kaizziizg <kaizz1129@gmail.com>
2020-05-23 16:28:37 +0800

    fix txt

---
+++ data/creat_array.py
@@ -0,0 +1,26 @@
+import csv 
+import io
+china_term=[]
+taiwan_term=[]
+#刪除第一行，然後儲存刪除後的檔案
+with open("googlesheet.csv",'r',encoding="utf-8") as f:
+    with open("googlesheet_n.csv",'w') as f1:
+        next(f) # skip header line
+        for line in f:
+            f1.write(line)
+#讀取新檔案
+with open('googlesheet_n.csv', newline='') as csvfile:
+    rows = csv.DictReader(csvfile)
+    for row in rows:
+        print(row['台灣用語'],row['中國用語/其他'])
+        taiwan_term.append(row['台灣用語'])
+        china_term.append(row['中國用語/其他'])
+#建立txt
+with io.open("taiwan_term.txt",'w', encoding='utf8') as file:
+    for i in taiwan_term:    
+        file.write(i)
+        file.write(",")
+with io.open("china_term.txt",'w', encoding='utf8') as file:
+    for i in china_term:    
+        file.write(i)
+        file.write(",")
